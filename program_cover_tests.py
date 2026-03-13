bb16de8f27ca4063b4937b61251b50a12fe25194
radubobe <46399340+radubobe@users.noreply.github.com>
2026-03-13 19:27:28 +0200

    Added the Python file for running the algorithm using program coverage strategy.

---
+++ program_cover_tests.py
@@ -0,0 +1,29 @@
+from learning_algorithm import LearningAutomata
+from ComputeUtil import ComputeUtil
+from tabulate import tabulate
+alphabet = ['b', 'c', 'd', 'e', 'f', 'k', 'l']
+language = ['ld','bcd','effk']
+maximumLength = max(list(map(lambda a: len(a), language)))
+learningAutomata = LearningAutomata(alphabet, language, 4)
+
+learningAutomata.compute()
+Q, F, table = learningAutomata.buildAutomaton()
+print("Q:",Q)
+print("F:",F)
+print("Table: ",table)
+#print(tabulate(table, headers=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r'],tablefmt='grid'))
+s_set = learningAutomata.s_set
+w_set = learningAutomata.w_set
+observation_table = learningAutomata.get_table(s_set, w_set)
+print("Observation table: ", observation_table)
+new_s_set = ComputeUtil.compute_s_set(s_set, observation_table)
+print("Print new_s_set:",new_s_set)
+observation_table_new_s_set = learningAutomata.get_table(new_s_set, w_set)
+print("Print observation table new_s_set:",observation_table_new_s_set)
+new_w_set = ComputeUtil.compute_w_set(w_set, observation_table_new_s_set)
+print("Print new_w_set:",new_w_set)
+u_set = ComputeUtil.compute_u_set_with_letters(s_set=new_s_set, alphabet=alphabet, w_set=new_w_set)
+u_set = list(filter(lambda a: len(a) <= maximumLength, u_set))
+u_set_sort = sorted(u_set, key=lambda a: (len(a), a))
+u_set_sort.remove('')
+print("Print u_set:", u_set)
